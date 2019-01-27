b5abd8531c02491025c7f81ca6660a62fa4c4aaa
trimute2 <ben.j.stern@gmail.com>
2019-01-27 18:32:50 -0500

    added some stuff so that at run time the move data only works with one curve

--- Assets/None Plugins/Scripts/MoveData.cs
+++ Assets/None Plugins/Scripts/MoveData.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 #if UNITY_EDITOR
+using UnityEditor;
 using UnityEditor.Animations;
 #endif
 
@@ -12,19 +13,21 @@ public class MoveData : ScriptableObject {
 	name of the variable in the Move editor*/
 #if UNITY_EDITOR
 	public AnimatorController controller;
+	public bool valid;
 #endif
 	public string animationStateName;
 	public float length;
 	public float frameRate;
-	public float playBackSpeed;
+	public float playBackSpeed = 1;
 
 	/// <summary>the flags effected by the move</summary>
 	[EnumFlags]
 	public CommonFlags commonFlagsEffected;
 
 	public AnimationCurve[] commonFlagsCurves = new AnimationCurve[Enum.GetValues(typeof(CommonFlags)).Length-1];
+	private AnimationCurve combinedCurve;
 
-	/*TODO: its possible that instead of saving a list of curve I save one that is parsed into several. but figuring out how to do that could be a head ache*/
+	/*TODO: its possible that instead of saving a list of curves I save one that is parsed into several. but figuring out how to do that could be a head ache*/
 
 	// Update is called once per frame
 	void Update () {
@@ -33,6 +36,7 @@ public class MoveData : ScriptableObject {
 
 	//for functions only called in the editor
 #if UNITY_EDITOR
+	/// <summary> Resizes the array of animation curves <para /> used by move editor when a new flag is added</summary>
 	public void ResizeCurves()
 	{
 		AnimationCurve[] temp = commonFlagsCurves;
@@ -41,6 +45,58 @@ public class MoveData : ScriptableObject {
 		{
 			commonFlagsCurves[i] = temp[i];
 		}
-}
+	}
+	/// <summary> Used to verrify that the move will work</summary>
+	public void Validate()
+	{
+
+		//TODO: write function;
+		#region curveStuff
+		combinedCurve = new AnimationCurve();
+		//get what keyframes exist at what times
+		List<float> keyFrames = new List<float>();
+		for(int i = 0; i < commonFlagsCurves.Length; i++)
+		{
+			for(int j = 0; j<commonFlagsCurves[i].keys.Length; j++)
+			{
+				float time = commonFlagsCurves[i].keys[j].time;
+				bool contains = false;
+				for(int k = 0; k < keyFrames.Count; k++)
+				{
+					if(keyFrames[k] == time)
+					{
+						contains = true;
+						break;
+					}
+				}
+				if (!contains)
+				{
+					keyFrames.Add(time);
+				}
+			}
+		}
+		//evaluate all the key frames across all the curves and add to the combined curve
+		for(int i = 0; i < keyFrames.Count; i++)
+		{
+			Keyframe key = new Keyframe(keyFrames[i], 0);
+			for(int k = 0; k < commonFlagsCurves.Length; k++)
+			{
+				int flag = (1 << k);
+				if ((k & flag) == flag)
+				{
+					key.value += flag * (int)(commonFlagsCurves[k].Evaluate(key.time));
+				}
+			}
+			combinedCurve.AddKey(key);
+		}
+		//set all the key frames so that the tangent mode is clamped
+		for(int i = 0; i < combinedCurve.keys.Length; i++)
+		{
+			AnimationUtility.SetKeyLeftTangentMode(combinedCurve, i, AnimationUtility.TangentMode.Constant);
+			AnimationUtility.SetKeyRightTangentMode(combinedCurve, i, AnimationUtility.TangentMode.Constant);
+		}
+		#endregion curveStuff
+
+	}
 #endif
 }
