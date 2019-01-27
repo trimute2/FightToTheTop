0dc74bae5d646190b89594dd059714789b232b74
trimute2 <ben.j.stern@gmail.com>
2019-01-27 01:32:17 -0500

    worked on the move editor somemore

--- Assets/None Plugins/Scripts/MoveData.cs
+++ Assets/None Plugins/Scripts/MoveData.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 [CreateAssetMenu(fileName = "Move", menuName = "Data/Move Data", order = 1)]
 public class MoveData : ScriptableObject {
 	/*WARNING: if you change he names of any of these variables also change the
 	name of the variable in the Move editor*/
 #if UNITY_EDITOR
-	public CharacterController controller;
+	public AnimatorController controller;
 #endif
 	public string animationStateName;
 	public float length;
+	public float frameRate;
 	public float playBackSpeed;
 
 	public AnimationCurve test;
--- Assets/None Plugins/Scripts/MoveEditor.cs
+++ Assets/None Plugins/Scripts/MoveEditor.cs
@@ -2,16 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Animations;
 
+[CustomEditor(typeof(MoveData))]
 public class MoveEditor : Editor {
 	SerializedProperty animationName;
 	SerializedProperty length;
 	SerializedProperty speed;
+	SerializedProperty controllerProp;
+	List<AnimatorState> stateList;
+	bool validName = true;
 
 	private void OnEnable()
 	{
+		stateList = new List<AnimatorState>();
 		animationName = serializedObject.FindProperty("animationStateName");
 		length = serializedObject.FindProperty("length");
 		speed = serializedObject.FindProperty("playBackSpeed");
+		controllerProp = serializedObject.FindProperty("controller");
+		UpdateStateList();
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+		EditorGUILayout.PropertyField(controllerProp, new GUIContent("Controller"));
+		if (GUI.changed)
+		{
+			UpdateStateList();
+		}
+		EditorGUI.BeginChangeCheck();
+		if (!validName)
+		{
+			GUI.backgroundColor = Color.red;
+		}
+		else
+		{
+			GUI.backgroundColor = Color.white;
+		}
+		EditorGUILayout.PropertyField(animationName, new GUIContent("State Name"));
+		if (EditorGUI.EndChangeCheck())
+		{
+			validName = false;
+			int index = GetStateIndex(animationName.stringValue);
+			if(index != -1)
+			{
+				AnimationClip clip = stateList[index].motion as AnimationClip;
+				if(clip != null)
+				{
+					length.floatValue = clip.length;
+					validName = true;
+				}
+			}
+		}
+		serializedObject.ApplyModifiedProperties();
+		//base.OnInspectorGUI();
+	}
+
+	/**<summary>updates the list of states</summary>*/
+	private void UpdateStateList()
+	{
+		stateList.Clear();
+		AnimatorController controller = (AnimatorController)controllerProp.objectReferenceValue;
+		if (controller != null)
+		{
+			AnimatorControllerLayer[] layers = controller.layers;
+			for(int i = 0; i < layers.Length; i++)
+			{
+				StatesFromMachine(layers[i].stateMachine);
+			}
+		}
+	}
+	/**<summary>recercively checks sub state machines for states</summary>*/
+	private void StatesFromMachine(AnimatorStateMachine stateMachine)
+	{
+		ChildAnimatorStateMachine[] stateMachines = stateMachine.stateMachines;
+		for(int i = 0; i < stateMachines.Length; i++)
+		{
+			StatesFromMachine(stateMachines[i].stateMachine);
+		}
+		ChildAnimatorState[] states = stateMachine.states;
+		for(int i = 0; i< states.Length; i++)
+		{
+			stateList.Add(states[i].state);
+		}
+	}
+
+	private int GetStateIndex(string name)
+	{
+		for(int i = 0; i < stateList.Count; i++)
+		{
+			if(stateList[i].name == name)
+			{
+				return i;
+			}
+		}
+		return -1;
 	}
 }
