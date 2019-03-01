37edc162fdd8cdd3aa9f09611515dfea8b13ee65
trimute2 <ben.j.stern@gmail.com>
2019-03-01 13:50:21 -0500

    removed unnecessary variables from enemy

--- Assets/None Plugins/Scripts/Enemy.cs
+++ Assets/None Plugins/Scripts/Enemy.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnemyCommands
-{
-	None = 0,
-	Idle,
-	MoveIn,
-	MakeSpace,
-	Attack,
-}
-
 public class Enemy : EntityController {
 	//the structure of enemy is currently unorganized
 	//currently changing it so that it will be organized around this tutorial
@@ -18,30 +9,13 @@ public class Enemy : EntityController {
 	private bool active;
 	public float longRange;
 	public float midRange;
-	//does this have permision to go to mid range
-	protected bool engageMidRange = false;
 	public float closeRange;
-	//does this enemy have permission to go to close range
-	protected bool engageCloseRange = false;
-	private int targetRange = 2;
+	//does this enemy have permission to go to close 
+	private int targetRange = Target.LONG_RANGE;
 	protected int currentTargetRange = Target.OUT_RANGE;
-	private float decisionRange;
 	protected Avoider avoider;
 	private Vector3 avoidVec = Vector3.zero;
 	private bool attackPermission = false;
-	protected EnemyCommands currentCommand;
-
-	public EnemyCommands CurrentCommand
-	{
-		get
-		{
-			return currentCommand;
-		}
-		set
-		{
-			currentCommand = value;
-		}
-	}
 
 	public float baseAgression;
 	private float agro = 0;
@@ -55,7 +29,6 @@ public class Enemy : EntityController {
 	}
 
 	public int enemySize;
-
 	
 	/// <summary>
 	/// The target of the enemies attacks
@@ -90,54 +63,14 @@ public class Enemy : EntityController {
 			return xdistance;
 		}
 	}
-
-	/// <summary>
-	/// Is this enemy close enough to the player to start considering attacks
-	/// </summary>
-	private bool inDecisionRange;
-
-	protected AttackRange currentRange;
-
-	public AttackRange CurrentRange
-	{
-		get
-		{
-			return currentRange;
-		}
-		set
-		{
-			if(currentRange != value)
-			{
-				currentCommand = EnemyCommands.None;
-			}
-			currentRange = value;
-		}
-	}
-
-
-	/// <summary>
-	/// Does the manager need to send this enemy a command
-	/// </summary>
-	public bool ManagerDecision
-	{
-		get
-		{
-			return (inDecisionRange && currentMove == null);
-		}
-	}
 	
 	// Use this for initialization
 	public override void Start()
 	{
 		base.Start();
-		//TODO calculate decisionRange
-
 		active = false;
 		xdistance = float.MaxValue;
-		//temp decisionRange for test
-		decisionRange = 7.5f;
 		avoider = GetComponent<Avoider>();
-		currentCommand = EnemyCommands.Idle;
 	}
 
 	protected override Vector2 EntityUpdate(Vector2 previousTarget)
@@ -145,14 +78,12 @@ public class Enemy : EntityController {
 		targetVelocity = Vector2.zero;
 		if (!active || target == null)
 		{
-			inDecisionRange = false;
 			//if the enemy is not active or the target has not been set dont do anything
 			return Vector2.zero;
 		}
 		//update distance to target
 		xdistance = target.transform.position.x - transform.position.x;
 		int previousRange = currentTargetRange;
-		inDecisionRange = (Mathf.Abs(xdistance) <= decisionRange);
 		if((flagData.commonFlags & CommonFlags.CanTurn) != CommonFlags.None)
 		{
 			if(Mathf.Sign(xdistance) != facing)
@@ -238,21 +169,12 @@ public class Enemy : EntityController {
 	// Update is called once per frame
 	protected override void CheckMoves()
 	{
-		if(inDecisionRange)
-		{
-			
-		}
+
 	}
 
 	protected virtual void EnemyDecision()
 	{
 		targetRange = Target.MID_RANGE;
-		/*
-		switch (currentRange)
-		{
-			case AttackRange.Close =
-
-		}*/
 	}
 
 	//TODO: either add more functinallity to this or make target range protected
