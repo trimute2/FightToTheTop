2a25d2442e8c4251e2c14a54a4fa52f5932a75e3
trimute2 <ben.j.stern@gmail.com>
2019-03-13 16:14:10 -0400

    more work on grunt

--- Assets/None Plugins/Scripts/GruntComp.cs
+++ Assets/None Plugins/Scripts/GruntComp.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(Targeter))]
 [RequireComponent(typeof(EntityController))]
 public class GruntComp : MonoBehaviour {
-
+	public float movementSpeed = 4.5f;
 	private MoveHandler moveHandler;
 	private Targeter targeter;
 	private EntityControllerComp entityController;
@@ -19,6 +19,56 @@ public class GruntComp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Target target = targeter.target;
+		if (target != null)
+		{
+			switch (targeter.CurrentRange)
+			{
+				case Target.LONG_RANGE:
+					//Try and move in
+					if (target.RequestTargeterCount(Target.CLOSE_RANGE) < 1)
+					{
+						targeter.TargetRange = Target.CLOSE_RANGE;
+					}
+					else if (target.RequestEnemyCount(Target.MID_RANGE, targeter.Direction) < 2)
+					{
+						targeter.TargetRange = Target.MID_RANGE;
+					}
+					else
+					{
+						targeter.TargetRange = Target.LONG_RANGE;
+						//shouldAvoid = true;
+
+						//any long range attack decisions go here
+					}
+					break;
+				case Target.MID_RANGE:
+					if (target.RequestEnemyCount(Target.CLOSE_RANGE) < 1)
+					{
+						targeter.TargetRange = Target.CLOSE_RANGE;
+					} //else if(target.RequestEnemyCount(Target.MID_RANGE) > 4)
+					else if (target.RequestEnemyCount(Target.MID_RANGE, targeter.Direction) > 2)
+					{
+						targeter.TargetRange = Target.LONG_RANGE;
+					}
+					else
+					{
+						targeter.TargetRange = Target.MID_RANGE;
+						//shouldAvoid = true;
+					}
+					break;
+				case Target.CLOSE_RANGE:
+					if (target.RequestEnemyRemaining(Target.CLOSE_RANGE) > 1)
+					{
+						targeter.TargetRange = Target.MID_RANGE;
+					}
+					else
+					{
+						targeter.TargetRange = Target.CLOSE_RANGE;
+						//attackPermission = true;
+					}
+					break;
+			}
+		}
 	}
 }
