095e32ec6d6a93076a1cb936e5a2101ee7448f54
trimute2 <ben.j.stern@gmail.com>
2019-03-13 15:57:50 -0400

    started on grunt component

---
+++ Assets/None Plugins/Scripts/GruntComp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveHandler))]
+[RequireComponent(typeof(Targeter))]
+[RequireComponent(typeof(EntityController))]
+public class GruntComp : MonoBehaviour {
+
+	private MoveHandler moveHandler;
+	private Targeter targeter;
+	private EntityControllerComp entityController;
+	// Use this for initialization
+	void Start () {
+		moveHandler = GetComponent<MoveHandler>();
+		targeter = GetComponent<Targeter>();
+		entityController = GetComponent<EntityControllerComp>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
