3aeea800672b5ca08482544499040159dfa3af4e
trimute2 <ben.j.stern@gmail.com>
2019-03-19 14:09:18 -0400

    started working on adding visual effects

---
+++ Assets/None Plugins/Scripts/EffectsScripts/GruntShootEffect.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisualEffects
+{
+	[RequireComponent(typeof(Animation))]
+	public class GruntShootEffect : MonoBehaviour, IVisualEffect
+	{
+		public LineRenderer midBeam;
+
+		private Transform aimPoint;
+		public Transform AimPoint
+		{
+			get
+			{
+				return aimPoint;
+			}
+			set
+			{
+				aimPoint = value;
+			}
+		}
+
+		private Transform originPoint;
+		public Transform OriginPoint
+		{
+			get
+			{
+				return originPoint;
+			}
+			set
+			{
+				originPoint = value;
+			}
+		}
+
+		public void EffectHook(List<Vector3> points, List<Transform> transforms)
+		{
+			originPoint = transforms[0];
+			aimPoint = transforms[1];
+		}
+
+		private void LateUpdate()
+		{
+			midBeam.SetPosition(0, originPoint.position);
+			midBeam.SetPosition(1, aimPoint.position);
+		}
+
+		public void EndEffect()
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/None Plugins/Scripts/EffectsScripts/IVisualEffect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisualEffects
+{
+	public interface IVisualEffect 
+	{
+		void EffectHook(List<Vector3> points, List<Transform> transforms);
+	}
+}
