2b6c9eb6fbfbb236a262196de4416c6c3372de80
Ben Stern (RIT Student) <bjs2904@ad.rit.edu>
2019-03-26 16:05:19 -0400

    Added UIManager which will update health slider

---
+++ Assets/None Plugins/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public Slider healthSlider;
+	public HealthComponent player;
+	// Start is called before the first frame update
+	void Start()
+    {
+		healthSlider.maxValue = player.maxHealth;
+		healthSlider.value = player.maxHealth;
+		player.HealthUpdateEvent += UpdateHealthSlider;
+	}
+
+	void UpdateHealthSlider()
+	{
+		healthSlider.value = player.Health;
+	}
+}
