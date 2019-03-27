0da3d7590abb7114c804a0da67a9c8ac3ca9634f
trimute2 <ben.j.stern@gmail.com>
2019-03-27 18:54:39 -0400

    created component to handle enemy deaths

---
+++ Assets/None Plugins/Scripts/ComponentScripts/EnemyDeathHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthComponent))]
+public class EnemyDeathHandler : MonoBehaviour
+{
+	private HealthComponent healthComponent;
+	private EntityControllerComp entityController;
+	private bool isDead;
+    // Start is called before the first frame update
+    void Start()
+    {
+		healthComponent = GetComponent<HealthComponent>();
+		entityController = GetComponent<EntityControllerComp>();
+		isDead = false;
+		healthComponent.OnDeath += AtZeroHP;
+    }
+
+	private void AtZeroHP()
+	{
+		isDead = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (isDead)
+		{
+			//todo: ensure that the death animation has finished
+			if(entityController != null)
+			{
+				if (entityController.Grounded)
+				{
+					Destroy(gameObject);
+				}
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
+    }
+}
