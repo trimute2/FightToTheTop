61f9ebe806feb25573767a8d492bc4e1d7206b7b
trimute2 <ben.j.stern@gmail.com>
2019-04-10 19:41:24 -0400

    Basic animations setup

--- Assets/None Plugins/Scripts/RenderOrderComp.cs
+++ Assets/None Plugins/Scripts/RenderOrderComp.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-[RequireComponent(typeof(Renderer))]
 public class RenderOrderComp : MonoBehaviour
 {
 	public int orderInLayer;
